b83c419e95297d123e3b1a28954061fe5e13b94f
saipatil29 <psai63443@gmail.com>
2026-06-22 14:45:46 +0530

    sai

---
+++ src/Main.kt
@@ -0,0 +1,28 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var i:Int = 4
+    var j:Float = 70f
+    var c: Char = 'T'
+    var l: String = "sai"
+    var b: Boolean = false
+    var d: Double = 25.35
+    var m: Long = 25554554L
+    var s: Short = -5
+    var x: Byte = 125
+    val co=60
+
+
+    println("i = +i")
+    println("j = +j")
+    println("c = +c")
+    println("l = +l")
+    println("b = +b")
+    println("d = +d")
+    println("m = +m")
+    println("s = +s")
+    println("x = $x")
+    println("co = +co")
+}
+
+
